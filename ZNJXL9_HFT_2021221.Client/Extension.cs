166877fc4ea77f561c7a78bf80e9839f3985ca52
Necrozgul <merse.sarvari@gmail.com>
2021-10-18 12:18:05 +0200

    recreating.Client

---
+++ ZNJXL9_HFT_2021221.Client/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZNJXL9_HFT_2021221.Client
+{
+    static class Extension
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string str)
+        {
+            Console.WriteLine("*** BEGIN " + str);
+            foreach (T item in input)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("*** END " + str);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ ZNJXL9_HFT_2021221.Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using ZNJXL9_HFT_2021221.Client;
+using ZNJXL9_HFT_2021221.Data;
+using ZNJXL9_HFT_2021221.Repository;
+
+namespace ZNJXL9_HFT_2021221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ProbaKiiratas:");
+            XYZDbContext x = new XYZDbContext();
+
+
+
+            var starshipData = from starship in x.Starships
+                                            join brand in x.Brand
+                                            on starship.BrandId equals brand.Id
+                                            join weapon in x.Weapon
+                                            on starship.WeaponId equals weapon.Id
+                                            select new { Model = starship.Model, Id = starship.Id , Brand = brand.Name, Weapon = weapon.Name };
+
+
+            var brandData = from brand in x.Brand select new { Id = brand.Id, Name = brand.Name };
+
+            var weaponData = from weapon in x.Weapon select new { Id = weapon.Id, Name = weapon.Name };
+
+
+            starshipData.ToConsole("starships");
+
+            //x.StarshipNameChange(x, 1, "Probaname");
+            //Működik
+            StarshipRepository s = new StarshipRepository(x);
+            BrandRepository b = new BrandRepository(x);
+            WeaponRepository w = new WeaponRepository(x);
+        }
+    }
+}
