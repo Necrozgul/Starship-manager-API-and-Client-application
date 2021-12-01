e80b7196abf1bf8ee7c7a71856bb447262a5b251
Necrozgul <merse.sarvari@gmail.com>
2021-12-01 19:57:08 +0100

    addign try catch to the console app

--- ZNJXL9_HFT_2021221.Client/Program.cs
+++ ZNJXL9_HFT_2021221.Client/Program.cs
@@ -64,14 +64,14 @@ namespace ZNJXL9_HFT_2021221.Client
                         Console.WriteLine(info);
                         break;
                     case ConsoleKey.D4:
-                        info = ListItems(restService);
+                        ListItems(restService);
                         Console.WriteLine(info);
                         break;
                     case ConsoleKey.A:
                         MenuAVGPrice(restService);
                         Console.WriteLine(info);
                         break;
-                    case ConsoleKey.M:
+                    case ConsoleKey.Z:
                         MenuMostExpensiveStarship(restService);
                         Console.WriteLine(info);
                         break;
@@ -81,6 +81,14 @@ namespace ZNJXL9_HFT_2021221.Client
                         break;
                     case ConsoleKey.B:
                         MenuAveragePriceByBrand(restService);
+                        Console.WriteLine(info);
+                        break;
+                    case ConsoleKey.M:
+                        MenuAveragePriceByModel(restService);
+                        Console.WriteLine(info);
+                        break;
+                    case ConsoleKey.W:
+                        MenuAveragePriceByWeapon(restService);
                         Console.WriteLine(info);
                         break;
                     case ConsoleKey.C:
@@ -105,8 +113,11 @@ namespace ZNJXL9_HFT_2021221.Client
             Console.WriteLine("[3] Remove item.");
             Console.WriteLine("[4] List of the specific items");
             Console.WriteLine("[A] Average Starship Price");
-            Console.WriteLine("[M] Most Expensive Starship");
+            Console.WriteLine("[Z] Most Expensive Starship");
             Console.WriteLine("[E] Cheapest starship");
+            Console.WriteLine("[B] AVG Price - Brand");
+            Console.WriteLine("[M] AVG Price - Model");
+            Console.WriteLine("[W] AVG Price - Weapon");
             Console.WriteLine("[B] AveragePriceByBrand");
             Console.WriteLine("[c] Clear the Console");
             Console.WriteLine("[x] stop the program");
@@ -114,142 +125,253 @@ namespace ZNJXL9_HFT_2021221.Client
         }
         static void MenuInsertItem(RestService restService)
         {
-            Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
-            string tabblename = Console.ReadLine();
-            if (tabblename == "starship")
+            try
             {
-                Console.WriteLine("Starship name: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Weapon id: ");
-                int weaponid = int.Parse(Console.ReadLine());
-                Console.WriteLine("Brand id: ");
-                int brandid = int.Parse(Console.ReadLine());
-                Console.WriteLine("Price: ");
-                int price = int.Parse(Console.ReadLine());
-                restService.Post(new Starship() { Model=name, BrandId = brandid, WeaponId = weaponid, BasePrice = price}, tabblename);
-            }
-            else if (tabblename == "brand")
-            {
-                Console.WriteLine("Brand Name: ");
-                string name = Console.ReadLine();
-                restService.Post(new Brand() { Name = name }, tabblename);
-            }
-            else if (tabblename == "brand")
-            {
-                Console.WriteLine("Weapon Name: ");
-                string name = Console.ReadLine();
-                restService.Post(new Weapon() { Name = name }, tabblename);
-            }
-            else
-            {
-                throw new ArgumentException("There is no table with that name");
+                Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
+                string tabblename = Console.ReadLine();
+                if (tabblename == "starship")
+                {
+                    Console.WriteLine("Starship name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Weapon id: ");
+                    int weaponid = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Brand id: ");
+                    int brandid = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Price: ");
+                    int price = int.Parse(Console.ReadLine());
+                    restService.Post(new Starship() { Model = name, BrandId = brandid, WeaponId = weaponid, BasePrice = price }, tabblename);
+                }
+                else if (tabblename == "brand")
+                {
+                    Console.WriteLine("Brand Name: ");
+                    string name = Console.ReadLine();
+                    restService.Post(new Brand() { Name = name }, tabblename);
+                }
+                else if (tabblename == "brand")
+                {
+                    Console.WriteLine("Weapon Name: ");
+                    string name = Console.ReadLine();
+                    restService.Post(new Weapon() { Name = name }, tabblename);
+                }
+                else
+                {
+                    throw new ArgumentException("There is no table with that name");
 
+                }
             }
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
         static void MenuModifyItem(RestService restService)
         {
-            Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
-            string tabblename = Console.ReadLine();
-            if (tabblename == "starship")
+            try
             {
-                Console.WriteLine("[Editing]\nType id here:");
-                int id = int.Parse(Console.ReadLine());
-                Console.WriteLine("Starship name: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Weapon id: ");
-                int weaponid = int.Parse(Console.ReadLine());
-                Console.WriteLine("Brand id: ");
-                int brandid = int.Parse(Console.ReadLine());
-                Console.WriteLine("Price: ");
-                int price = int.Parse(Console.ReadLine());
-                restService.Put(new Starship() { Id = id, Model = name, BrandId = brandid, WeaponId = weaponid, BasePrice = price }, tabblename);
-            }
-            else if (tabblename == "brand")
-            {
-                Console.WriteLine("[Editing]\nType id here:");
-                int id = int.Parse(Console.ReadLine());
-                Console.WriteLine("Brand Name: ");
-                string name = Console.ReadLine();
-                restService.Put(new Brand() { Id = id, Name = name }, tabblename);
-            }
-            else if (tabblename == "weapon")
-            {
-                Console.WriteLine("[Editing]\nType id here:");
-                int id = int.Parse(Console.ReadLine());
-                Console.WriteLine("Weapon Name: ");
-                string name = Console.ReadLine();
-                restService.Post(new Weapon() { Id = id, Name = name }, tabblename);
-            }
-            else
-            {
-                throw new ArgumentException("There is no table with that name");
+                Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
+                string tabblename = Console.ReadLine();
+                if (tabblename == "starship")
+                {
+                    Console.WriteLine("[Editing]\nType id here:");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Starship name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Weapon id: ");
+                    int weaponid = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Brand id: ");
+                    int brandid = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Price: ");
+                    int price = int.Parse(Console.ReadLine());
+                    restService.Put(new Starship() { Id = id, Model = name, BrandId = brandid, WeaponId = weaponid, BasePrice = price }, tabblename);
+                    Console.WriteLine("Editing was succesfull!");
+                }
+                else if (tabblename == "brand")
+                {
+                    Console.WriteLine("[Editing]\nType id here:");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Brand Name: ");
+                    string name = Console.ReadLine();
+                    restService.Put(new Brand() { Id = id, Name = name }, tabblename);
+                    Console.WriteLine("Editing was succesfull!");
+                }
+                else if (tabblename == "weapon")
+                {
+                    Console.WriteLine("[Editing]\nType id here:");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Weapon Name: ");
+                    string name = Console.ReadLine();
+                    restService.Put(new Weapon() { Id = id, Name = name }, tabblename);
+                    Console.WriteLine("Editing was succesfull!");
+                }
+                else
+                {
+                    throw new ArgumentException("There is no table with that name");
 
+                }
             }
-
+            catch (Exception ex)
+            {
+                Console.WriteLine("Az adott elem módosítása sikertelen!\n"+ex.Message);
+            }
         }
         static void MenuRemoveItem(RestService restService)
         {
-            Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
-            string tabblename = Console.ReadLine();
-            Console.WriteLine("\nType the id of the item you want to remove");
-            int id = int.Parse(Console.ReadLine());
-            restService.Delete(id, tabblename);
+            try
+            {
+                Console.WriteLine("\nGive the name of the table: (starship,brand,weapon)");
+                string tabblename = Console.ReadLine();
+                Console.WriteLine("\nType the id of the item you want to remove");
+                int id = int.Parse(Console.ReadLine());
+                restService.Delete(id, tabblename);
+                Console.WriteLine("Removing was succesfull!");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("[Error]"+ex.Message);
+            }
+            
         }
-        static string ListItems(RestService restService) 
+        static void ListItems(RestService restService) 
         {
-            Console.WriteLine("\nGive the name of the table you want to list: (starship,brand,weapon)");
-            string tabblename = Console.ReadLine();
-            string data_ = "";
-            if (tabblename == "starship")
+            try
             {
-               var datas = restService.Get<Starship>("starship");
-                foreach (Starship data in datas)
+                Console.WriteLine("\nGive the name of the table you want to list: (starship,brand,weapon)");
+                string tabblename = Console.ReadLine();
+                string data_ = "";
+                if (tabblename == "starship")
                 {
-                    data_ += $"\n{data.ToString()}";
+                    var datas = restService.Get<Starship>("starship");
+                    foreach (Starship data in datas)
+                    {
+                        data_ += $"\n{data.ToString()}";
+                    }
                 }
+                if (tabblename == "brand")
+                {
+                    var datab = restService.Get<Brand>("brand");
+                    foreach (Brand data in datab)
+                    {
+                        data_ += $"\n{data.ToString()}";
+                    }
+                }
+                if (tabblename == "weapon")
+                {
+                    var dataw = restService.Get<Weapon>("weapon");
+                    foreach (Weapon data in dataw)
+                    {
+                        data_ += $"\n{data.ToString()}";
+                    }
+                }
+                Console.WriteLine(data_);
             }
-            if (tabblename == "brand")
+            catch (Exception ex)
             {
-                var datab = restService.Get<Brand>("brand");
-                foreach (Brand data in datab)
-                {
-                    data_ += $"\n{data.ToString()}";
-                }
+                Console.WriteLine("[Listing Failed] "+ex.Message);
             }
-            if (tabblename == "weapon")
-            {
-                var dataw = restService.Get<Weapon>("weapon");
-                foreach (Weapon data in dataw)
-                {
-                    data_ += $"\n{data.ToString()}";
-                }
-            }
-            return data_;
         }
         static void MenuAveragePriceByBrand(RestService restService) 
         {
-            var x=
-                restService.Get<KeyValuePair<string, double>> ("stat/averagepricebybrand");
-            Console.WriteLine(x);
+            try
+            {
+                Console.WriteLine("\nAVG Price by Brand:");
+                var x =
+                    restService.Get<KeyValuePair<string, double>>("stat/avgpricebybrand");
+                string text = "";
+                foreach (var item in x)
+                {
+                    text += ($"\n[{item.Key}] Price: {item.Value}");
+                }
+                Console.WriteLine(text);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"[Error] {ex.Message}");
+            }
+        }
+        static void MenuAveragePriceByModel(RestService restService)
+        {
+            try
+            {
+                Console.WriteLine("\nAVG Price by Model:");
+                var x =
+                    restService.Get<KeyValuePair<string, double>>("stat/avgpricebymodel");
+                string text = "";
+                foreach (var item in x)
+                {
+                    text += ($"\n[{item.Key}] Price: {item.Value}");
+                }
+                Console.WriteLine(text);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine($"[Error] {ex.Message}");
+            }
+        }
+        static void MenuAveragePriceByWeapon(RestService restService)
+        {
+            
+            try
+            {
+                Console.WriteLine("\nAVG Price by Weapon:");
+                var x =
+                    restService.Get<KeyValuePair<string, double>>("stat/avgpricebyweapon");
+                string text = "";
+                foreach (var item in x)
+                {
+                    text += ($"\n[{item.Key}] Price: {item.Value}");
+                }
+                Console.WriteLine(text);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"[Error] {ex.Message}");
+            }
         }
         static void MenuMostExpensiveStarship(RestService restService)
         {
-            Starship x = restService.GetSingle<Starship>("stat/mostexpensivestarship");
-            Console.WriteLine("\nThe most expensive starship:");
-            Console.WriteLine(x);
+            try
+            {
+                Starship x = restService.GetSingle<Starship>("stat/mostexpensivestarship");
+                Console.WriteLine("\nThe most expensive starship:");
+                Console.WriteLine(x);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[Error] "+ex.Message);
+            }         
         }
         static void MenuCheapestStarship(RestService restService)
         {
-            Starship x = restService.GetSingle<Starship>("stat/cheapeststarship");
-            Console.WriteLine("\nthe Ceapest Starship:");
-            Console.WriteLine(x);
+            try
+            {
+                Starship x = restService.GetSingle<Starship>("stat/cheapeststarship");
+                Console.WriteLine("\nthe Ceapest Starship:");
+                Console.WriteLine(x);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("[Error] "+ex.Message);
+            }
+            
         }
         static void MenuAVGPrice(RestService restService)
         {
-            var x = restService.GetSingle<double>("stat/avgprice");
-            Console.WriteLine("Average price of the starships:");
-            Console.WriteLine(x);
+            try
+            {
+                var x = restService.GetSingle<double>("stat/avgprice");
+                Console.WriteLine("Average price of the starships:");
+                Console.WriteLine(x);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("[Error] "+ex.Message);
+            }
+            
         }
     }
 }
