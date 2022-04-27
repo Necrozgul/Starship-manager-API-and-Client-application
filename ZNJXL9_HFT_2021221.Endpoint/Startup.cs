1fe2c9873706d8618453584a594c7a28e9c85248
Necrozgul <merse.sarvari@gmail.com>
2022-04-27 11:34:18 +0200

    adding new databse and deleting swagger

--- ZNJXL9_HFT_2021221.Endpoint/Startup.cs
+++ ZNJXL9_HFT_2021221.Endpoint/Startup.cs
@@ -43,31 +43,11 @@ namespace ZNJXL9_HFT_2021221.Endpoint
             services.AddSignalR();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ZNJXL9_HFT_2021221.Endpoint", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ZNJXL9_HFT_2021221.Endpoint v1"));
-            }
-
-            app.UseExceptionHandler(c => c.Run(async context =>
-            {
-                var exception = context.Features
-                    .Get<IExceptionHandlerPathFeature>()
-                    .Error;
-                var response = new { Msg = exception.Message };
-                await context.Response.WriteAsJsonAsync(response);
-            }));
-
 
             app.UseRouting();
 
